cc43081f74a40fe8e5d6c75ec3bacad19ad43e6c
claire <claire@claires-MacBook-Pro.local>
2025-01-15 10:09:15 -0700

    Project structure first draft

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "turnout-db",
+	Short: "Simple CLI to manage organizing turnout lists",
+	Long: `turnout-db is a CLI tool that wraps sqlite and web APIs for
+commonly-used organizing tools, such as ActionNetwork and Zoom, to streamline
+turnout list creation, prioritization, and management`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.turnout-db.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
